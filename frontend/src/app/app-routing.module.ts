cc2c212d0278e96c474e8c064d4fdb2d3e62fe62
Zordraknova <novel_rn@oulook.com>
2023-06-15 21:46:04 +0600

    1062023

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostPageComponent } from './post-page/post-page.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+import { CreatePostComponent } from './admin/create-post/create-post.component';
+import { LoginComponent } from './admin/login/login.component';
+
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'new_post', component: CreatePostComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,20 +1,41 @@
 import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { MaterialModule } from './mat.module';
+import { HomeComponent } from './home/home.component';
+import { PostPageComponent } from './post-page/post-page.component';
+import { ProfileComponent } from './profile/profile.component';
+import { CreatePostComponent } from './admin/create-post/create-post.component';
+import { EditPostComponent } from './admin/edit-post/edit-post.component';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
+import { LoaderComponent } from './@shared/loader/loader.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './admin/login/login.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PostPageComponent,
+    ProfileComponent,
+    CreatePostComponent,
+    EditPostComponent,
+    DashboardComponent,
+    LoaderComponent,
+    LoginComponent,
+
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     AppRoutingModule,
     MaterialModule,
+    FormsModule,
     BrowserAnimationsModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: !isDevMode(),
